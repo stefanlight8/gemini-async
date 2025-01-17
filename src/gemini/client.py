d377c838bb72d30628c996401a90da7f837ee852
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2025-01-17 14:36:14 +0300

    Use http client in Gemini client and improve generate content method

--- src/gemini/client.py
+++ src/gemini/client.py
@@ -1,40 +1,55 @@
+from asyncio.events import AbstractEventLoop, get_event_loop
+from collections.abc import Sequence
+from typing import Any
+
+from aiohttp import ClientResponse
 from aiohttp.client import ClientSession
 from msgspec.json import decode, encode
 
-from gemini.consts import API_VERSION_STRING, MODEL_METHOD_URL, MODEL_STRING, USER_AGENT
+from gemini.consts import API_VERSION_STRING, MODEL_METHOD_URL, MODEL_STRING
 from gemini.enums import ModelMethod
+from gemini.http_client import HTTPClient
 from gemini.structs import Content, GenerateContentResponse, Part
 
 
 class Gemini:
+    __slots__: Sequence[str] = ("__logger", "_key", "_loop", "_http", "version", "model")
+
     def __init__(
         self,
         key: str,
         *,
         version: API_VERSION_STRING = "v1",
         model: MODEL_STRING = "gemini-1.5-pro",
+        loop: AbstractEventLoop | None = None,
         session: ClientSession | None = None,
     ) -> None:
         self._key: str = key
-        self._session: ClientSession | None = session
+        self._loop: AbstractEventLoop | None = loop or get_event_loop()
+        self._http: HTTPClient = HTTPClient(self._loop, session=session)
         self.version: API_VERSION_STRING = version
         self.model: MODEL_STRING = model
 
-    @property
-    def session(self) -> ClientSession:
-        if not self._session:
-            self._session = ClientSession()
-        return self._session
-
-    async def generate_content(self, text: str) -> GenerateContentResponse:
-        try:
-            async with self.session.post(
-                MODEL_METHOD_URL.format(
-                    version=self.version, model=self.model, method=ModelMethod.GENERATE_CONTENT.value, key=self._key
-                ),
-                headers={"Content-Type": "application/json", "User-Agent": USER_AGENT},
-                data=encode({"contents": [Content(parts=[Part(text)])]}),
-            ) as response:
-                return decode(await response.read(), type=GenerateContentResponse)
-        except Exception:
-            raise
+    async def generate_content(
+        self,
+        *content: Content,
+        version: API_VERSION_STRING | None = None,
+        model: MODEL_STRING | None = None,
+        system_instruction: str | None = None,  # only text available for system instruction (Google)
+    ) -> GenerateContentResponse | None:
+        body: dict[str, Any] = {"contents": content}
+        if system_instruction:
+            body["system_instruction"] = Content(parts=[Part(text=system_instruction)])
+        response: ClientResponse | None = await self._http.request(
+            "POST",
+            MODEL_METHOD_URL.format(
+                version=version or self.version,
+                model=model or self.model,
+                method=ModelMethod.GENERATE_CONTENT.value,
+                key=self._key,
+            ),
+            data=encode(body),
+        )
+        if not response:
+            return
+        return decode(await response.read(), type=GenerateContentResponse)
